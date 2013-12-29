3d934a58a9593a6cf84f0d04ef3eae516bfb86be
Joshua Kaplan <ylkaplan@gmail.com>
2013-12-28 20:54:21 -0500

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,21 +7,21 @@ class User < ActiveRecord::Base
   	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   							uniqueness: { case_sensitive: false }
 
-  	validates :password, length: { minimum: 6 }
+	validates :password, length: { minimum: 6 }
 
-  	has_secure_password
+	has_secure_password
 
-  	def User.new_remember_token
-  		SecureRandom.urlsafe_base64
-  	end
+	def User.new_remember_token
+		SecureRandom.urlsafe_base64
+	end
 
-  	def User.encrypt(token)
-  		Digest::SHA1.hexdigest(token.to_s)
-  	end
+	def User.encrypt(token)
+		Digest::SHA1.hexdigest(token.to_s)
+	end
 
-  	private
+	private
 
-  		def create_remember_token
-  			self.remember_token = User.encrypt(User.new_remember_token)
-  		end
+		def create_remember_token
+			self.remember_token = User.encrypt(User.new_remember_token)
+		end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,36 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
+include ApplicationHelper
+
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
   else
-    "#{base_title} | #{page_title}"
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
   end
 end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :be_signed_in do |message|
+	match do |page|
+		expect(page).to have_title(user.name)
+		expect(page).to have_link('Profile', href: user_path(user))
+		expect(page).to have_link('Sign out', href: signout_path)
+		expect(page).to_not have_link('Sign in', href: signin_path)
+	end
+end
+
+
+
+
+
+
